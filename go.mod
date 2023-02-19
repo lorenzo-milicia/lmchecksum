967abd6e2cf979b1e801f37883221f3df573e75f
Lorenzo Milicia <info@lorenzomilicia.dev>
2023-02-19 08:59:11 +0100

    Add other hashing functions handling

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go.lorenzomilicia.dev/checksum-checker
 
 go 1.19
 
-require go.lorenzomilicia.dev/libs v1.0.6
+require go.lorenzomilicia.dev/libs v1.1.2
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"crypto"
+	_ "crypto/md5"
+	_ "crypto/sha1"
 	_ "crypto/sha256"
-	"log"
+	_ "crypto/sha512"
+	"fmt"
 	"os"
 
 	"go.lorenzomilicia.dev/libs/checksum"
@@ -13,13 +16,36 @@ func main() {
 	args := os.Args
 
 	if len(args) < 3 {
-		log.Fatal("Not enough arguments passed")
+		fmt.Printf("Not enough arguments passed\n")
+		os.Exit(1)
 	}
 	file, err := os.ReadFile(args[1])
-
 	if err != nil {
-		log.Fatalf("Cannot read file %v", args[1])
+		fmt.Printf("Cannot read file %v\n", args[1])
+		os.Exit(1)
 	}
 
-	checksum.Checksum(file, args[2], crypto.SHA256)
+	algorithm := crypto.SHA3_256
+
+	if len(args) > 3 {
+		switch hf := args[3]; hf {
+		case "sha256":
+		case "sha1":
+			algorithm = crypto.SHA1
+		case "md5":
+			algorithm = crypto.MD5
+		case "sha512":
+			algorithm = crypto.SHA3_512
+		default:
+			fmt.Printf("Hashing algorithm %v not supported.\n", hf)
+			os.Exit(1)
+		}
+	}
+
+	if res := checksum.Checksum(file, args[2], algorithm); res == true {
+		fmt.Print("It checks out ✅\n")
+	} else {
+		fmt.Printf("Checksums don't match ❌\n")
+	}
+	os.Exit(0)
 }
