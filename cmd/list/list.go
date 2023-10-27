d9bf289f50f4916ac771a25c8e520e2c058658c5
Lorenzo Milicia <info@lorenzomilicia.dev>
2023-10-27 17:45:27 +0200

    Minor refactoring (fix build)

--- cmd/list/list.go
+++ cmd/list/list.go
@@ -4,7 +4,7 @@ import (
 	"crypto"
 	"fmt"
 	"github.com/spf13/cobra"
-	"go.lorenzomilicia.dev/lmchecksum/app"
+	cmd2 "go.lorenzomilicia.dev/lmchecksum/cmd"
 	"regexp"
 	"strings"
 )
@@ -15,7 +15,7 @@ var Command = &cobra.Command{
 	Long:  "Print list of available hash functions",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, h := range app.AvailableHashFunctions {
+		for _, h := range cmd2.AvailableHashFunctions {
 			str := regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(crypto.Hash(h).String(), "")
 			fmt.Println(strings.ToLower(str))
 		}
